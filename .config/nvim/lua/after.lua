978c57431b0cd650228c8f33abfd95e3799d17c8
belaja-akacija <oselskyi@protonmail.com>
2023-07-22 03:55:33 -0400

    add plugin-specific configuration file

---
+++ .config/nvim/lua/after.lua
@@ -0,0 +1,35 @@
+local keymap = vim.api.nvim_set_keymap;
+
+require'nvim-treesitter.configs'.setup {
+  ensure_installed = {"lua", "commonlisp"},
+  auto_install = true,
+  highlight = {
+    enable = true,
+    additional_vim_regex_highlighting = true
+  },
+}
+
+-- vlime window settings
+
+vim.cmd[[
+let g:vlime_window_settings = {
+  \ "arglist": {
+    \ "pos": "botright",
+    \ "size": 1,
+    \ "vertical": v:false
+  \ },
+  \ "repl": {
+    \ "size": 7
+  \ },
+  \ "sldb": {
+    \ "size": 7
+\ }
+\ }
+]]
+
+-- Harpoon settings
+
+keymap('n', '<C-t>', ':lua require("harpoon.ui").nav_next()<CR>', {noremap = true, silent = true});
+keymap('n', '<C-n>', ':lua require("harpoon.ui").nav_prev()<CR>', {noremap = true, silent = true});
+keymap('n', '<C-c>', ':lua require("harpoon.ui").toggle_quick_menu()<CR>', {noremap = true, silent = true});
+keymap('n', '<C-g>', ':lua require("harpoon.mark").add_file()<CR>', {noremap = true, silent = true});
